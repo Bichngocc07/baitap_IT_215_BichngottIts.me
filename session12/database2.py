dac5f69bcd5698a030854f4d4afb0d3e17172880
Hà Bích Ngọc <ngoc01072006@gmail.com>
2026-07-16 20:46:58 +0700

    Reformat database2.py for consistency

--- session12/database2.py
+++ session12/database2.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Nhớ thay đổi "password" theo đúng cấu hình MySQL của bạn
 DATABASE_URL = "mysql+pymysql://root:password@localhost:3306/connect_db"
 
 engine = create_engine(
@@ -13,10 +12,9 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# Dependency quản lý phiên làm việc, tự thu hồi kết nối tránh rò rỉ
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
-        db.close()  # KHẮC PHỤC TRIỆT ĐỂ: Giải phóng kết nối vật lý
+        db.close() 
