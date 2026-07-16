2bd4a87c8422bf9f7d35d63ffaa7b4dfcb54f70d
Hà Bích Ngọc <ngoc01072006@gmail.com>
2026-07-16 20:43:52 +0700

    Update database1.py

--- session12/database1.py
+++ session12/database1.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Thay đổi tài khoản/mật khẩu MySQL cho đúng với máy của bạn
 DATABASE_URL = "mysql+pymysql://root:123456@localhost:3306/connect_db"
 
 engine = create_engine(
@@ -13,10 +12,9 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# Dependency: Tạo kết nối và tự động giải phóng về Pool sau khi dùng xong
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
-        db.close()  # Đóng kết nối bắt buộc
+        db.close() 
